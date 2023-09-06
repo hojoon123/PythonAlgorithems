ff1433a5d71443d0f6f2b44b4a4da49ac3cc275a
hojun <rhzn5512@naver.com>
2023-09-06 17:45:47 +0900

    2493 탑

--- 2.자료구조/230906_BJODS/BJO2493스택.py
+++ 2.자료구조/230906_BJODS/BJO2493스택.py
@@ -3,19 +3,19 @@ input = stdin.readline
 
 def check(len_top, top_list):
     stack = []
-    result = []
+    receive_top_list = []
     
     for i in range(len_top):
         while stack:
             if stack[-1][1] > top_list[i]:  # 수신 가능한 상황
-                result.append(stack[-1][0] + 1)
+                receive_top_list.append(stack[-1][0] + 1)
                 break
             else:
                 stack.pop()
         if not stack:  # 스택이 비면 레이저를 수신할 탑이 없다.
-            result.append(0)
+            receive_top_list.append(0)
         stack.append([i, top_list[i]])  # 인덱스, 값
-    return (" ".join(map(str, result)))
+    return (" ".join(map(str, receive_top_list)))
                 
 if __name__ == '__main__':
     n = int(input())
@@ -30,4 +30,5 @@ if __name__ == '__main__':
 6 9 5 7 4
 예제 출력 1 
 0 0 2 2 4
+
 '''
